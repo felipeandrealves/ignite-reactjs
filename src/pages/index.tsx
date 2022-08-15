a04bf8ca0e426a65352832d98e9ec6a32c6070f2
Felipe Andre Alves <felipe.alves@synvia.com>
2022-08-15 16:39:46 -0300

    feat: create login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,61 @@
+import {
+  Button,
+  Flex,
+  Input,
+  Stack,
+  FormLabel,
+  FormControl,
+} from "@chakra-ui/react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <h1>DashGo</h1>;
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              name="email"
+              id="email"
+              type="email"
+              bgColor="gray.900"
+              focusBorderColor="pink.500"
+              variant="filled"
+              size="lg"
+              _hover={{ bgColor: "gray.900" }}
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              name="password"
+              id="password"
+              type="password"
+              bgColor="gray.900"
+              focusBorderColor="pink.500"
+              variant="filled"
+              size="lg"
+              _hover={{ bgColor: "gray.900" }}
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Home;
