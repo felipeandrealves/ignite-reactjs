44ba0d7e836bac3d479a18740485423c30956685
Felipe Andre Alves <felipe.alves@synvia.com>
2022-08-16 14:53:47 -0300

    refactor: accept ref at input component

--- src/components/form/input/index.tsx
+++ src/components/form/input/index.tsx
@@ -4,12 +4,16 @@ import {
   Input as ChakraInput,
   InputProps,
 } from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
 
 interface IInputProps extends InputProps {
   label?: string;
 }
 
-export const Input = ({ name, label, ...props }: IInputProps) => {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, IInputProps> = (
+  { name, label, ...props },
+  ref
+) => {
   return (
     <FormControl>
       {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
@@ -20,9 +24,12 @@ export const Input = ({ name, label, ...props }: IInputProps) => {
         focusBorderColor="pink.500"
         variant="filled"
         size="lg"
+        ref={ref}
         _hover={{ bgColor: "gray.900" }}
         {...props}
       />
     </FormControl>
   );
 };
+
+export const Input = forwardRef(InputBase);
