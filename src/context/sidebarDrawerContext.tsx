b338bdd14ec695849332b6826e438f726db4e566
Felipe Andre Alves <felipe.alves@synvia.com>
2022-08-16 11:18:09 -0300

    feat: create sidebar context

---
+++ src/context/sidebarDrawerContext.tsx
@@ -0,0 +1,30 @@
+import { createContext, ReactNode, useContext, useEffect } from "react";
+import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+
+interface ISidebarDrawerProviderProps {
+  children: ReactNode;
+}
+
+type SidebarDrawerContextData = UseDisclosureReturn;
+
+const SidebarDrawerContext = createContext({} as SidebarDrawerContextData);
+
+export const SidebarDrawerProvider = ({
+  children,
+}: ISidebarDrawerProviderProps) => {
+  const disclosure = useDisclosure();
+  const { asPath } = useRouter();
+
+  useEffect(() => {
+    disclosure.onClose();
+  }, [asPath, disclosure]);
+
+  return (
+    <SidebarDrawerContext.Provider value={disclosure}>
+      {children}
+    </SidebarDrawerContext.Provider>
+  );
+};
+
+export const useSidebarDrawer = () => useContext(SidebarDrawerContext);
