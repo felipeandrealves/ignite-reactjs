c0ddad0d81929ef130a058d1991d3232d661f089
Felipe Andre Alves <felipe.alves@synvia.com>
2022-08-16 11:19:04 -0300

    feat: add way to open sidebar

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,15 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
 
+import { useSidebarDrawer } from "../../context/sidebarDrawerContext";
 import { NotificationNav } from "./notificationNav";
 import { SearchBox } from "./searchBox";
 import { Profile } from "./profile";
 import { Logo } from "./logo";
 
 export const Header = () => {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({ base: false, lg: true });
 
   return (
@@ -19,6 +23,17 @@ export const Header = () => {
       px="6"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          onClick={onOpen}
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          mr="2"
+        ></IconButton>
+      )}
+
       <Logo />
 
       {isWideVersion && <SearchBox />}
