9107d7b5febbcf9170fd159aa5253e6b626338aa
Felipe Andre Alves <felipe.alves@synvia.com>
2022-08-17 09:40:31 -0300

    feat: loading when refaching

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -46,6 +46,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
 
             <Link href="/users/create" passHref>
